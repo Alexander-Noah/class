887f46f529644756fa1f582eeacaff2a4eb13d72
Noah <zhenghaotao2005@163.com>
2025-06-05 16:08:16 +0800

    多选框添加信息

--- src/login.java
+++ src/login.java
@@ -4,19 +4,45 @@ import javax.swing.*;
  * Created by JFormDesigner on Thu Jun 05 09:52:36 CST 2025
  */
 
-
-
 /**
  * @author zheng
  */
 public class login extends JFrame {
     public login() {
         initComponents();
+
+        // 添加数据到身份下拉框
+        comboBox1.addItem("学生");
+        comboBox1.addItem("教师");
+        comboBox1.addItem("管理员");
+
+        // 添加数据到班级下拉框
+        comboBox2.addItem("计算机科学与技术1班");
+        comboBox2.addItem("计算机科学与技术2班");
+        comboBox2.addItem("软件工程1班");
+        comboBox2.addItem("软件工程2班");
+        comboBox2.addItem("网络工程1班");
+
+        this.setBounds(100, 100, 450, 400);
+        this.setVisible(true);
+       /*
+        // 从数据库获取身份选项
+        List<String> roles = database.getRoles();
+        for (String role : roles) {
+        comboBox1.addItem(role);
+        }
+
+        // 从数据库获取班级选项
+        List<String> classes = database.getClasses();
+        for (String cls : classes) {
+            comboBox2.addItem(cls);
+        }
+        */
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        label1 = new JLabel();
+        title = new JLabel();
         label2 = new JLabel();
         label3 = new JLabel();
         button1 = new JButton();
@@ -28,81 +54,67 @@ public class login extends JFrame {
         label5 = new JLabel();
         comboBox2 = new JComboBox();
 
-        //======== this ========
-        var contentPane = getContentPane();
-        contentPane.setLayout(null);
-
-        //---- label1 ----
-        label1.setText("\u75ab\u60c5\u9632\u63a7\u767b\u5165\u7cfb\u7edf");
-        label1.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 24));
-        contentPane.add(label1);
-        label1.setBounds(155, 18, 225, 30);
-
-        //---- label2 ----
-        label2.setText("\u8d26\u53f7");
-        label2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
-        contentPane.add(label2);
-        label2.setBounds(85, 70, 52, 35);
-
-        //---- label3 ----
-        label3.setText("\u5bc6\u7801");
-        label3.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
-        contentPane.add(label3);
-        label3.setBounds(80, 115, 54, 35);
-
-        //---- button1 ----
-        button1.setText("\u767b\u5165");
-        button1.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
-        contentPane.add(button1);
-        button1.setBounds(274, 266, 95, 45);
-        contentPane.add(textField1);
-        textField1.setBounds(150, 70, 227, textField1.getPreferredSize().height);
-        contentPane.add(passwordField1);
-        passwordField1.setBounds(150, 115, 227, passwordField1.getPreferredSize().height);
-
-        //---- button2 ----
-        button2.setText("\u6ce8\u518c");
-        button2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
-        contentPane.add(button2);
-        button2.setBounds(127, 265, 90, 45);
-        contentPane.add(comboBox1);
-        comboBox1.setBounds(150, 160, 222, comboBox1.getPreferredSize().height);
-
-        //---- label4 ----
-        label4.setText("\u8eab\u4efd");
-        label4.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
-        contentPane.add(label4);
-        label4.setBounds(85, 167, 42, 28);
-
-        //---- label5 ----
-        label5.setText("\u73ed\u7ea7");
-        label5.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
-        contentPane.add(label5);
-        label5.setBounds(85, 218, 43, 28);
-        contentPane.add(comboBox2);
-        comboBox2.setBounds(150, 210, 222, comboBox2.getPreferredSize().height);
-
+        //======== tab1 ========
         {
-            // compute preferred size
-            Dimension preferredSize = new Dimension();
-            for(int i = 0; i < contentPane.getComponentCount(); i++) {
-                Rectangle bounds = contentPane.getComponent(i).getBounds();
-                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
-                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
-            }
-            Insets insets = contentPane.getInsets();
-            preferredSize.width += insets.right;
-            preferredSize.height += insets.bottom;
-            contentPane.setMinimumSize(preferredSize);
-            contentPane.setPreferredSize(preferredSize);
+            this.getContentPane().setLayout(null);
+
+            //---- label1 ----
+            title.setText("疫情防控登入系统");
+            title.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 24));
+            this.getContentPane().add(title);
+            title.setBounds(155, 18, 225, 30);
+
+            //---- label2 ----
+            label2.setText("账号");
+            label2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
+            this.getContentPane().add(label2);
+            label2.setBounds(85, 70, 52, 35);
+
+            //---- label3 ----
+            label3.setText("密码");
+            label3.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
+            this.getContentPane().add(label3);
+            label3.setBounds(80, 115, 54, 35);
+
+            //---- button1 ----
+            button1.setText("登入");
+            button1.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
+            this.getContentPane().add(button1);
+            button1.setBounds(274, 266, 95, 45);
+            this.getContentPane().add(textField1);
+            textField1.setBounds(150, 70, 227, textField1.getPreferredSize().height);
+            this.getContentPane().add(passwordField1);
+            passwordField1.setBounds(150, 115, 227, passwordField1.getPreferredSize().height);
+
+            //---- button2 ----
+            button2.setText("注册");
+            button2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
+            this.getContentPane().add(button2);
+            button2.setBounds(127, 265, 90, 45);
+            this.getContentPane().add(comboBox1);
+            comboBox1.setBounds(150, 165, 222, comboBox1.getPreferredSize().height);
+
+            //---- label4 ----
+            label4.setText("身份");
+            label4.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
+            this.getContentPane().add(label4);
+            label4.setBounds(85, 167, 42, 28);
+
+            //---- label5 ----
+            label5.setText("班级");
+            label5.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
+            this.getContentPane().add(label5);
+            label5.setBounds(85, 218, 43, 28);
+            this.getContentPane().add(comboBox2);
+            comboBox2.setBounds(150, 213, 222, comboBox2.getPreferredSize().height);
+
+            this.getContentPane().setPreferredSize(new Dimension(460, 365));
         }
-        pack();
-        setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    private JLabel label1;
+    private JLabel title;
     private JLabel label2;
     private JLabel label3;
     private JButton button1;
@@ -114,4 +126,8 @@ public class login extends JFrame {
     private JLabel label5;
     private JComboBox comboBox2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new login());
+    }
 }
