ff07b1d22f0d7f90fff171160049b10f6dc722fe
Noah <zhenghaotao2005@163.com>
2025-06-06 20:12:02 +0800

    添加功能

--- src/Enroll.java
+++ src/Enroll.java
@@ -6,76 +6,117 @@ import javax.swing.*;
 
 /**
  * @author zheng
- * 废弃代码，勿动！！！
- * 废弃代码，勿动！！！
- * 废弃代码，勿动！！！
- * 废弃代码，勿动！！！
- * 废弃代码，勿动！！！
+ * 注册页面
+ * 注册页面
+ * 注册页面
+ * 注册页面
  */
 public class Enroll extends JFrame {
+    //把变量放这，方便调用
+    private JLabel Title;
+    private JLabel TextUser;
+    private JLabel PassworldText;
+    private JLabel PassworldTest2;
+    private JTextField UserField;
+    private JPasswordField passwordText;
+    private JPasswordField passwordText2;
+    private JButton RegisterButton;
+    private JLabel ClassTxt;
+    private JComboBox<String> ChooseClass;
+    private JComboBox<String> ChooseID;
+    private JLabel IDTxt;
     public Enroll() {
+        //初始化界面
+        initFrame();
+        //初始化组件
         initComponents();
+        //窗口居中显示
+        this.setLocationRelativeTo(null);
+        //关闭窗口程序停止
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //显示界面
         this.setVisible(true);
+
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        //标题文本
-        Title = new JLabel();
-        //账号文本
-        TextUser = new JLabel();
-        //密码文本
-        PassworldText = new JLabel();
-        //确认密码文本
-        PassworldTest2 = new JLabel();
-        //账号输入
-        UserField = new JTextField();
-        //密码
-        passwordText = new JPasswordField();
-        //确认密码
-        passwordText2 = new JPasswordField();
-        //注册按钮
-        RegisterButton = new JButton();
+
 
         //======== this ========
         var contentPane = getContentPane();
         contentPane.setLayout(null);
 
         //---- Title ----
-        Title.setText("疫情防控注册系统（学生端）");
+        Title.setText("疫情防控注册系统");
         Title.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 22));
         contentPane.add(Title);
-        Title.setBounds(145, 12, 300, 38);
+        Title.setBounds(145, 10, 300, 38);
 
         //---- TextUser ----
         TextUser.setText("学号");
         TextUser.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
         contentPane.add(TextUser);
-        TextUser.setBounds(83, 70, 46, 30);
+        TextUser.setBounds(84, 65, 46, 30);
 
         //---- PassworldText ----
         PassworldText.setText("密码");
         PassworldText.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
         contentPane.add(PassworldText);
-        PassworldText.setBounds(84, 120, 46, 30);
+        PassworldText.setBounds(84, 115, 46, 30);
 
         //---- passwordText ----
         PassworldTest2.setText("确认密码");
         PassworldTest2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
         contentPane.add(PassworldTest2);
-        PassworldTest2.setBounds(49, 169, 81, 30);
+        PassworldTest2.setBounds(49, 165, 81, 30);
+
+        //----UserField----
         contentPane.add(UserField);
-        UserField.setBounds(151, 65, 280, UserField.getPreferredSize().height);
+        UserField.setBounds(145, 65, 280, 30);
+
+        //----passwordText----
         contentPane.add(passwordText);
-        passwordText.setBounds(151, 115, 280, passwordText.getPreferredSize().height);
+        passwordText.setBounds(145, 115, 280, 30);
+
+        //----passwordText2----
         contentPane.add(passwordText2);
-        passwordText2.setBounds(150, 166, 280, passwordText2.getPreferredSize().height);
+        passwordText2.setBounds(145, 165, 280, 30);
+
+        //---- IDTxt ----
+        IDTxt.setText("身份");
+        IDTxt.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
+        this.getContentPane().add(IDTxt);
+        IDTxt.setBounds(75, 215, 42, 28);
+
+        //---- ChooseID ----
+        this.getContentPane().add(ChooseID);
+        ChooseID.setBounds(150, 215, 225, 28);
+        // 添加数据到身份下拉框
+        ChooseID.addItem("学生");
+        ChooseID.addItem("教师");
+
+        //----ClassTxt----
+        ClassTxt.setText("班级");
+        ClassTxt.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 18));
+        this.getContentPane().add(ClassTxt);
+        ClassTxt.setBounds(84, 265, 42, 28);
+
+        //---- ChooseClass ----
+        this.getContentPane().add(ChooseClass);
+        ChooseClass.setBounds(151, 265, 280,30);
+        // 添加数据到班级下拉框
+        ChooseClass.addItem("计算机科学与技术1班");
+        ChooseClass.addItem("计算机科学与技术2班");
+        ChooseClass.addItem("软件工程1班");
+        ChooseClass.addItem("软件工程2班");
+        ChooseClass.addItem("网络工程1班");
 
         //---- RegisterButton ----
         RegisterButton.setText("注册");
         RegisterButton.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 20));
         contentPane.add(RegisterButton);
-        RegisterButton.setBounds(162, 231, 240, RegisterButton.getPreferredSize().height);
+        RegisterButton.setBounds(162, 315, 240, RegisterButton.getPreferredSize().height);
 
         {
             // compute preferred size
@@ -97,15 +138,40 @@ public class Enroll extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    private JLabel Title;
-    private JLabel TextUser;
-    private JLabel PassworldText;
-    private JLabel PassworldTest2;
-    private JTextField UserField;
-    private JPasswordField passwordText;
-    private JPasswordField passwordText2;
-    private JButton RegisterButton;
+
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+    private void initFrame(){
+        //标题文本
+        Title = new JLabel();
+        //账号文本
+        TextUser = new JLabel();
+        //账号输入
+        UserField = new JTextField();
+        //密码文本
+        PassworldText = new JLabel();
+        //密码输入
+        passwordText = new JPasswordField();
+        //确认密码文本
+        PassworldTest2 = new JLabel();
+        //确认密码输入
+        passwordText2 = new JPasswordField();
+        //身份选择框
+        ChooseID = new JComboBox<>();
+        //身份文本
+        IDTxt= new JLabel();
+        //班级文本
+        ClassTxt = new JLabel();
+        //班级选择框
+        ChooseClass = new JComboBox<>();
+        //注册按钮
+        RegisterButton = new JButton();
+        //设置界面大小
+        this.setBounds(100, 100, 450, 350);
+        //取消默认的布局
+        this.getContentPane().setLayout(null);
+        //禁止调整界面大小
+        this.setResizable(false);
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Enroll());
     }
--- src/UItest.java
+++ src/UItest.java
@@ -1,6 +1,6 @@
 class UITest {
     public static void main(String[] args) {
         //测试类 0表示学生，1表示教师
-        new UI(0);
+        new UI(1);
     }
 }
